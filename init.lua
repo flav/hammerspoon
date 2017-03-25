a5bb36ee29265f6e6c5ad6ecc37bcb205d876a2f
Flavio daCosta <fdacosta@nutshell.com>
2017-03-24 21:51:35 -0400

    Initial commit (with weather 🌤)

---
+++ init.lua
@@ -0,0 +1,17 @@
+
+-- Use Control+` to reload Hammerspoon config
+hs.hotkey.bind({'ctrl'}, '`', nil, function()
+  hs.reload()
+end)
+
+
+local weather = require("hs-weather")
+weather.start({
+  geolocation= false,
+  location="Ann Arbor, MI",
+  refresh=1800,
+  units="F"
+})
+
+
+hs.notify.new({title='Hammerspoon', informativeText='Ready to rock 🤘'}):send()
