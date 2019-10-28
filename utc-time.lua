87c20b4e40960b797f8956337b093696df2b3303
Flavio daCosta <flav@binaryservice.com>
2019-10-28 15:15:18 -0400

    Timer was getting garbage collected
    Fix frozen utc clock

--- utc-time.lua
+++ utc-time.lua
@@ -1,6 +1,9 @@
 
 utcTime = hs.menubar.new()
-hs.timer.doEvery(1, function ()
+
+-- capture the response from `doEvery` so it will not be garbage collected
+-- http://www.hammerspoon.org/go/#variablelife
+utcTimer = hs.timer.doEvery(1, function ()
   utcTime:setTitle(os.date("!%H:%M Z"))
   utcTime:setTooltip('\n(' .. os.time() .. ')\n')
 end)
