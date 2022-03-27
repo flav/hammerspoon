c02e325fda71ea8385f07f23067377527826d145
Flavio daCosta <flav@binaryservice.com>
2022-03-27 14:51:06 -0400

    Prevent blank content until first timer loads

--- utc-time.lua
+++ utc-time.lua
@@ -1,9 +1,13 @@
 
 utcTime = hs.menubar.new()
 
--- capture the response from `doEvery` so it will not be garbage collected
--- http://www.hammerspoon.org/go/#variablelife
-utcTimer = hs.timer.doEvery(1, function ()
+local setTime = function ()
   utcTime:setTitle(os.date("!%H:%M Z"))
   utcTime:setTooltip('\n(' .. os.time() .. ')\n')
-end)
+end
+
+setTime()
+
+-- capture the response from `doEvery` so it will not be garbage collected
+-- http://www.hammerspoon.org/go/#variablelife
+utcTimer = hs.timer.doEvery(1, setTime)
